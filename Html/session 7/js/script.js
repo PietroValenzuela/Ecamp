def4d735bbf9ce2da1db256f793fdd95564f7151
PietroValenzuela <pietro.mv@icloud.com>
2024-07-18 13:10:21 -0400

    Prtimeros intentos JS
    Practica javaS

---
+++ Html/session 7/js/script.js
@@ -0,0 +1,12 @@
+/* console.log ("Hola desde la consola");*/
+console.log("Bienvenido a la calculadora Basica");
+var a;
+var b;
+var suma;
+//a=90331;
+a=prompt("Ingresar un Numero")
+//b=4233324;
+b=prompt("Ingresa otro Numero")
+suma= a + b;
+alert("el resultado de la suma es"+ suma);
+console.log(suma);
