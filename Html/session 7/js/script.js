46a9aa7a1cf674329d72af93b0d81bb56f542a92
PietroValenzuela <pietro.mv@icloud.com>
2024-07-19 12:35:33 -0400

    cambio
    session 7 total

--- Html/session 7/js/script.js
+++ Html/session 7/js/script.js
@@ -1,12 +1,58 @@
-/* console.log ("Hola desde la consola");*/
-console.log("Bienvenido a la calculadora Basica");
+/* console.log("Hola desde la consola JS");*/
+alert("Bienvenido a la calculadora Basica");
+console.log("Bienvenido a la calculador Basica");
+alert("Opciones: 1. Suma, 2. Resta , 3. Multiplicacion, 4. Division");
+opcion = parseInt(prompt("Escribe el numero de la opcion deseada"));
 var a;
 var b;
-var suma;
-//a=90331;
-a=prompt("Ingresar un Numero")
-//b=4233324;
-b=prompt("Ingresa otro Numero")
-suma= a + b;
-alert("el resultado de la suma es"+ suma);
-console.log(suma);
+a = parseInt(prompt("Ingresa un numero"));
+b = parseInt(prompt("Ingresa otro numero"));
+if (a == 0 && b == 0) {
+    alert("Los numero son 0 , nose puede hacer operaciones")
+} else {
+    switch (opcion) {
+        case 1:
+            total = suma(a, b);
+            alert("El resultado de la suma es " + total);
+            break;
+        case 2:
+            total2 = resta(a, b);
+            alert("El resultado de la resta es " + total2);
+            break;
+        case 3:
+            total3 = multiplicacion(a, b);
+            alert("El resultado de la multiplicacion es: " + total3);
+            break;
+        case 4:
+            total4 = division(a, b);
+            alert("El resultado de la division es " + total4);
+            break;
+        default:
+            alert("Opcion no valida, leer correctamente las opciones");
+            break;
+    }
+}
+
+alert("Gracias por hacer uso de la calculadora 0077");
+
+
+function suma(a, b) {
+    resultado = a + b;
+    return resultado;
+}
+function resta(a, b) {
+    resultado = a - b;
+    return resultado;
+}
+function multiplicacion(a, b) {
+    resultado = a * b;
+    return resultado;
+}
+function division(a, b) {
+    if (a > b) {
+        resultado = a / b;
+    } else {
+        resultado = 0;
+    }
+    return resultado;
+}
